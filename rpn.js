02e94839191ccbb00b934c338510ba0650dbbe30
chrismeeky <chrismeekymin@min.ng>
2021-09-18 03:05:24 -0400

    adds some helper functions

--- rpn.js
+++ rpn.js
@@ -1,3 +1,27 @@
 // initialize screen
+const screenTextElement = document.querySelector(".calculator__screen__text");
 
-document.querySelector(".calculator__screen__text").innerHTML = "0";
+function appendText(text) {
+  screenTextElement.innerText += text;
+}
+
+function removeLast(text) {
+  screenTextElement.innerText = screenTextElement.innerText.slice(
+    0,
+    screenTextElement.innerText.length - 1
+  );
+}
+
+function calculate(input) {
+  if (input === "<<") {
+    return removeLast();
+  }
+
+  appendText(input);
+}
+document
+  .querySelector(".calculator__buttons")
+  .addEventListener("click", (event) => {
+    const text = event.target.innerText;
+    calculate(text);
+  });
